adc3b4dde21bbf1be6d646b2efe4656f327067a3
DuyDucLH <duyduc.luonghuu@gmail.com>
2025-05-22 17:24:55 +0700

    chore: add elixir-ls

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "elixir", "eex", "heex" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+}
