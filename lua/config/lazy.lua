4d7f739c48f6576c315860cee48b0e792387ef46
DuyDucLH <duyduc.luonghuu@gmail.com>
2025-05-22 16:15:59 +0700

    chore: add github copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,11 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
+    -- LazyVim's default plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- LazyVim's extra plugins
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    -- Personal's plugins
     { import = "plugins.git" },
     { import = "plugins.ui" },
     { import = "plugins.theme" },
@@ -32,7 +36,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "monokaipro", "tokyonight", "habamax" } },
+  install = { colorscheme = { "monokaipro" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
