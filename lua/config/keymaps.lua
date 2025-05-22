4ac1fee9113d5c5e2138077d25ee095d42fda7b8
DuyDucLH <duyduc.luonghuu@gmail.com>
2025-05-22 17:51:01 +0700

    chore: init keymap setup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function map(modes, lhs, rhs, opts)
+  vim.keymap.set(modes, lhs, rhs, opts)
+end
+local function normal_map(lhs, rhs, opts)
+  map("n", lhs, rhs, opts)
+end
+local function insert_map(lhs, rhs, opts)
+  map("i", lhs, rhs, opts)
+end
+local function visual_map(lhs, rhs, opts)
+  map("v", lhs, rhs, opts)
+end
+local function terminal_map(lhs, rhs, opts)
+  map("t", lhs, rhs, opts)
+end
