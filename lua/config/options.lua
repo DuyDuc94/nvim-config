0914bbf593738f36b24294d43df615523fb0eafd
DuyDucLH <duyduc.luonghuu@gmail.com>
2025-05-23 09:35:06 +0700

    chore: setting cursor shape

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,10 @@
 vim.opt.wrap = true
 vim.opt.breakindent = true
 vim.opt.formatoptions:append("t")
+vim.opt.guicursor = {
+  "n-v-c:block", -- Normal, Visual, Command: block
+  "i-ci-ve:hor25", -- Insert, Insert command: vertical bar 25%
+  "r-cr:hor20", -- Replace: horizontal bar 20%
+  "o:hor50", -- Operator pending: horizontal bar
+  "a:blinkwait700-blinkoff400-blinkon250", -- blinking style
+}
