b4ae386241eb5c73aeb34f82d30a69fb9333f400
DuyDucLH <duyduc.luonghuu@gmail.com>
2025-06-03 16:38:18 +0700

    chore: update lualine config

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -1,1 +1,21 @@
-return {}
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {
+          function()
+            return os.date("%H:%M")
+          end,
+        },
+      }
+      return opts
+    end,
+  },
+}
