5ba37b7b98a3988d38af3c182e165fefe5d91e8c
DuyDucLH <duyduc.luonghuu@gmail.com>
2025-06-03 16:45:25 +0700

    chore: remove lsp config for elixir

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,18 +1,1 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        elixirls = {
-          cmd = { vim.fn.stdpath("data") .. "/mason/packages/elixir-ls/language_server.sh" },
-          settings = {
-            elixirLS = {
-              dialyzerEnabled = false,
-              fetchDeps = false,
-            },
-          },
-        },
-      },
-    },
-  },
-}
+return {}
